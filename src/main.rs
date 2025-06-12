3acdc0667b95a1d97753896e2ba8fd24a84a6e67
t1end4t <tiendatarse3004@gmail.com>
2025-06-12 12:08:29 +0700

    update code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use nn_zero_to_hero_rs::micrograd::Value;
 
 fn main() {
-    let a = Value { data: 3f32 };
+    let a = Value::new(-2.0);
+    let b = Value::new(4.0);
 
-    println!("{}", a);
+    let c = a + b;
+
+    println!("{}", c);
 }
--- src/micrograd/engine.rs
+++ src/micrograd/engine.rs
@@ -1,4 +1,4 @@
-use std::fmt::Display;
+use std::{fmt::Display, ops::Add};
 
 pub struct Value {
     pub data: f32,
@@ -6,6 +6,20 @@ pub struct Value {
 
 impl Display for Value {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.data)
+        write!(f, "Value: data: {}", self.data)
+    }
+}
+
+impl Value {
+    pub fn new(input_data: f32) -> Self {
+        Value { data: input_data }
+    }
+}
+
+impl Add for Value {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Value::new(self.data + rhs.data)
     }
 }
